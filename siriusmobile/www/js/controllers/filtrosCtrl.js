4359512cd4316c7f8e2af93db5086618d192ccc6
unknown <guilherme.sousa@pontalina.consinco.com.br>
2015-11-14 10:23:33 -0200

    Alterações no layout, início de geração do json de filtro

--- siriusmobile/www/js/controllers/filtrosCtrl.js
+++ siriusmobile/www/js/controllers/filtrosCtrl.js
@@ -9,6 +9,14 @@ angular.module('app.controllers')
     $scope.frequencia = 33;
     $scope.limitePadroes = 10;
     
+    $scope.dataInicial = new Date();
+    /*$scope.dataInicial.setMilliseconds(0);
+    $scope.dataInicial.setSeconds(0);*/
+    
+    $scope.dataFinal = new Date();
+    $scope.dataFinal.setMilliseconds(0);
+    $scope.dataFinal.setSeconds(0);
+    
     $scope.toggleGroup = function(grupo){
         if ($scope.mostraGrupo(grupo))
           $scope.grupo = null;
@@ -16,30 +24,35 @@ angular.module('app.controllers')
           $scope.grupo = grupo;
     };
     
-    $scope.togglePdv = function(nroEmpresa, index){
-        $scope.atualizaPDV(index);
-        if ($scope.mostraPdvs(nroEmpresa))
+    $scope.togglePdv = function(empresa){
+        $scope.atualizaPDV(empresa, false);
+        
+        if ($scope.mostraPdvs(empresa))
             $scope.empPdvVisivel = null;
         else
-            $scope.empPdvVisivel = nroEmpresa;
+            $scope.empPdvVisivel = empresa.nroempresa;
     }
     
-    $scope.atualizaPDV = function(index){
-        if($scope.empresas[index].listaPdv.length == 0){
-            $scope.getListaPDV($scope.empresas[index].nroempresa, index);
+    $scope.atualizaPDV = function(empresa, trazPdvsMarcados){
+        if(empresa.listaPdv.length == 0){
+            $scope.getListaPDV(empresa, trazPdvsMarcados);
         }
     };
     
-    $scope.mostraPdvs = function(nroEmpresa){
-        return $scope.empPdvVisivel === nroEmpresa;
+    $scope.mostraPdvs = function(empresa){
+        return $scope.empPdvVisivel === empresa.nroempresa;
     };
     
-    $scope.getListaPDV = function (nroEmpresa, index) {
-        DataService.url = ServerConfig.getServerUrl() + PDVService.url + nroEmpresa;
+    $scope.getListaPDV = function (empresa, trazPdvsMarcados) {
+        DataService.url = ServerConfig.getServerUrl() + PDVService.url + empresa.nroempresa;
         DataService.getData(
             function (dataResponse) {
-                if(dataResponse.length > 0)
-                    $scope.empresas[index].listaPdv = dataResponse;
+                if(dataResponse.length > 0){
+                    empresa.listaPdv = dataResponse;
+                    
+                    if(trazPdvsMarcados)
+                        for(var i in empresa.listaPdv) empresa.listaPdv[i].isChecked = true;
+                }
                 else{
                     $ionicPopup.alert({
                         title: "Ops",
@@ -103,6 +116,59 @@ angular.module('app.controllers')
     };
     
     $scope.gerarPadroes = function(){
+        var objFiltro = {};
+        var mensagens = [];    
+    
+        
+        if(!$scope.dataInicial) mensagens.push('Informe a data inicial');
+        if(!$scope.dataFinal) mensagens.push('Informe a data final');
+        if($scope.dataInicial > $scope.dataFinal) mensagens.push('A data final não pode ser menor que a data inicial.');
+        
+        objFiltro.dataInicial = $scope.dataInicial;
+        objFiltro.dataFinal   = $scope.dataFinal;
+        
+        objFiltro.diaSemana = [];
+        
+        objFiltro.frequencia = $scope.frequencia;
+        objFiltro.limitePadroes = $scope.limitePadroes;
+        
+        objFiltro.segmentos = [];
+        
+        //
+        
+        
+        $ionicPopup.alert({
+                    title: "Ops",
+                    template: "O servidor não respondeu, tente novamente mais tarde.",
+                    okText: "OK",
+                    okType: "button-balanced"
+                });
+        
+        
         $state.go('padroes.grafico');
+    };
+    
+    $scope.clickPdv = function(empresa, pdv){
+        if(pdv.isChecked){
+            empresa.isChecked = true;
+        }
+        else{
+            var existePdvMarcado = false;
+            
+            for(var i in empresa.listaPdv)
+                if(empresa.listaPdv[i].isChecked) existePdvMarcado = true;
+            
+            empresa.isChecked = existePdvMarcado
+        }
+    };
+    
+    $scope.clickEmpresa = function(empresa){
+        //empresa.listaPdv 
+        $scope.atualizaPDV(empresa, true);
+        
+        if(empresa.isChecked)
+            for(var i in empresa.listaPdv) empresa.listaPdv[i].isChecked = true;
+        else
+            for(var i in empresa.listaPdv) empresa.listaPdv[i].isChecked = false;
     };
 });
